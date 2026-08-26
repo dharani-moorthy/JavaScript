a4108ad5dbd848516caae541c4d56eebac04464d
Dharani M <dharanimoorthy51@gmail.com>
2026-08-26 20:13:27 +0530

    Switch

---
+++ Switch.js
@@ -0,0 +1,29 @@
+let n=2;
+switch(n){
+    case 1:
+        console.log("one");
+        break;
+    case 2:
+        console.log("It's Two");
+         break;
+    case 3:
+        console.log(" I am three");
+         break;
+    case 4:
+        console.log("I am Four");
+         break;
+    case 5:
+        console.log("Five");
+         break;
+    case 6:
+        console.log("six...");
+         break;
+    case 7:
+        console.log("seven!!!!");
+         break;
+    case 8:
+        console.log("eight");
+         break;
+    default:
+        console.log("Number is greater then 8");
+}
